7e36b86dbca8928ee7e2c39e97126b4e44f4431a
Bruno Neves <lebrunoneves@gmail.com>
2021-04-06 21:50:58 -0300

    Adiciona implementação do método de rasterização de primitivas gráficas

--- t1/src/basic.renderer.js
+++ t1/src/basic.renderer.js
@@ -5,15 +5,125 @@
 }(this, (function (exports) { 'use strict';
 
 
-        /* ------------------------------------------------------------ */
+    /* ------------------------------------------------------------ */
 
+    function ear_clipping(primitive) {
 
+        primitive.triangles = [];
+
+        let vertices = primitive.vertices;
+
+        let i = 0;
+        while(vertices.length >= 3) {
+            const prevVert = i-1 >= 0 ? vertices[i-1] : vertices[vertices.length-1];
+            const nextVert = i+1 < vertices.length ? vertices[i+1] : vertices[0];
+
+            const vectorA = prevVert.map((c, index) => c - vertices[i][index]);
+            const vectorB = nextVert.map((c, index) => c - vertices[i][index]);
+
+            if(angle_between(vectorA, vectorB) < Math.PI && !exists_vertice_inside_triangle(vertices, [prevVert, vertices[i], nextVert])) {
+                primitive.triangles.push([prevVert, vertices[i], nextVert]);
+                vertices.splice(i, 1);
+                i = 0;
+            }
+            else {
+                i++;
+            }
+            
+        }
+
+        inside(3, 2, primitive);
+    }
+
+    function scalar_product(a, b) {
+        return a[0]*b[0] + a[1]*b[1];
+    }
+
+    function cross_product(a, b) {
+        return a[0]*b[1] - a[1]*b[0];
+    }
+
+    function angle_between(a, b) {
+        const moduleProduct = Math.sqrt(a[0]*a[0] + a[1]*a[1]) * Math.sqrt(b[0]*b[0] + b[1]*b[1]);
+
+        const cos =  scalar_product(a, b) / moduleProduct;
+
+        return Math.acos(cos);
+    }
+
+    function exists_vertice_inside_triangle(vertices, triangle) {
+        
+        for (var v of vertices) {
+            
+            const vectorA = [v[0]-triangle[0][0], v[1] - triangle[0][1]];
+            const vectorEdgeA = [triangle[1][0]-triangle[0][0], triangle[1][1] - triangle[0][1]];
+
+            const vectorB = [v[0]-triangle[1][0], v[1] - triangle[1][1]];
+            const vectorEdgeB = [triangle[2][0]-triangle[1][0], triangle[2][1] - triangle[1][1]];
+
+            const vectorC = [v[0]-triangle[2][0], v[1] - triangle[2][1]];
+            const vectorEdgeC = [triangle[0][0]-triangle[2][0], triangle[0][1] - triangle[2][1]];
+
+            if(cross_product(vectorEdgeA, vectorA) > 0 &&
+                cross_product(vectorEdgeB, vectorB) > 0 &&
+                cross_product(vectorEdgeC, vectorC) > 0)
+                    return true;
+
+        }
+
+        return false;
+    }
+
+    function process_circle(primitive){
+        primitive.vertices = [primitive.center];
+
+        const h = primitive.center[0];
+        const k = primitive.center[1];
+        for (let i = 0; i < 2 * Math.PI ; i+=Math.PI/16 ) {
+            const x1 = primitive.radius * Math.cos(i) + h;
+            const x2 = primitive.radius * Math.sin(i) + k;
+            
+            primitive.vertices.push([x1, x2]);
+        }
+    }
+
+    function apply_xform(primitive) {
+        const A = primitive.xform;
+
+        for (let i = 0; i < primitive.vertices.length; i++) {
+            const p = [...primitive.vertices[i], 1];
+            
+            const q = [];
+            for (let j = 0; j < 3; j++) 
+                q.push(A[j][0]*p[0] + A[j][1]*p[1] + A[j][2]*p[2]);
+            
+            primitive.vertices[i] = [q[0], q[1]];
+        }
+    }
         
     function inside(  x, y, primitive  ) {
             // You should implement your inside test here for all shapes   
             // for now, it only returns a false test
-            
-            return false
+            const v = [x, y];
+
+            for (var triangle of primitive.triangles){
+                
+                const vectorA = [v[0]-triangle[0][0], v[1] - triangle[0][1]];
+                const vectorEdgeA = [triangle[1][0]-triangle[0][0], triangle[1][1] - triangle[0][1]];
+
+                const vectorB = [v[0]-triangle[1][0], v[1] - triangle[1][1]];
+                const vectorEdgeB = [triangle[2][0]-triangle[1][0], triangle[2][1] - triangle[1][1]];
+
+                const vectorC = [v[0]-triangle[2][0], v[1] - triangle[2][1]];
+                const vectorEdgeC = [triangle[0][0]-triangle[2][0], triangle[0][1] - triangle[2][1]];
+
+                if(cross_product(vectorEdgeA, vectorA) > 0 &&
+                    cross_product(vectorEdgeB, vectorB) > 0 &&
+                    cross_product(vectorEdgeC, vectorC) > 0)
+                        return true;
+            }
+
+            return false;
     }
         
     
@@ -53,6 +163,14 @@
          
                 // In this loop, the image attribute must be updated after the rasterization procedure.
                 for( var primitive of this.scene ) {
+
+                    if(primitive.shape === "circle")
+                        process_circle(primitive); 
+
+                    if(primitive.hasOwnProperty("xform"))
+                        apply_xform(primitive);
+
+                    ear_clipping(primitive);
 
                     // Loop through all pixels
                     // Use bounding boxes in order to speed up this loop
